e77b4d395e88362f6042951c46a682a5cf4aab2e
Bruno Jardón <bjardon97@gmail.com>
2020-06-28 03:51:27 -0500

    Updated port definition

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
  
 var app = express();
+const port = process.env.PORT || 3000;
  
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -18,4 +19,4 @@ app.use(express.static('assets'));
  
 app.use('/assets', express.static(__dirname + '/assets'));
  
-app.listen(3000);
+app.listen(port);
